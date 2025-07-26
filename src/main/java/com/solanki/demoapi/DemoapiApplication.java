81b9a8bbb976ecc01f25ec59321e2c4aebc17fa5
hiteshkumar042 <hksolanki78@gmail.com>
2025-07-26 17:35:23 +0530

    env setup

--- src/main/java/com/solanki/demoapi/DemoapiApplication.java
+++ src/main/java/com/solanki/demoapi/DemoapiApplication.java
@@ -2,11 +2,16 @@ package com.solanki.demoapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class DemoapiApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("DATABASE_URL", dotenv.get("DATABASE_URL"));
+		System.setProperty("DATABASE_USER", dotenv.get("DATABASE_USER"));
+		System.setProperty("DATABASE_PASSWORD", dotenv.get("DATABASE_PASSWORD"));
 		SpringApplication.run(DemoapiApplication.class, args);
 	}
 
