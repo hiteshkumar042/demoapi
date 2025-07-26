d08f1304e9c966e5cb2ddfa47e8b775813bdaff6
hiteshkumar042 <hksolanki78@gmail.com>
2025-07-26 19:41:15 +0530

    swagger ui and put mathod added

--- src/main/java/com/solanki/demoapi/Controller/StudentController.java
+++ src/main/java/com/solanki/demoapi/Controller/StudentController.java
@@ -2,7 +2,9 @@ package com.solanki.demoapi.Controller;
 
 import java.util.List;
 
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import com.solanki.demoapi.Model.Student;
 import com.solanki.demoapi.Repository.StudentRepository;
@@ -27,9 +29,32 @@ public class StudentController {
     }
 
     // DELETE a student by roll number
-    @DeleteMapping("/{id}")
-    public void deleteStudent(@PathVariable int id) {
-        studentRepository.deleteById(id);
+    @DeleteMapping("/{rollNo}")
+    public ResponseEntity<String> deleteStudent(@PathVariable int rollNo) {
+        if (studentRepository.existsById(rollNo)) {
+            studentRepository.deleteById(rollNo);
+            return ResponseEntity.ok("Student with rollNo " + rollNo + " has been deleted.");
+        } else {
+            return ResponseEntity.status(404).body("Student with rollNo " + rollNo + " not found.");
+        }
+    }
+
+    // Update Student by roll No
+    @PutMapping("/{rollNo}")
+    public Student update(@PathVariable int id, @RequestBody Student student) {
+        return studentRepository.save(student);
+    }
+
+    // get student by id
+    @GetMapping("/{rollNo}")
+    public ResponseEntity<?> getStudentById(@PathVariable int rollNo) {
+        Optional<Student> studentOptional = studentRepository.findById(rollNo);
+
+        if (studentOptional.isPresent()) {
+            return ResponseEntity.ok(studentOptional.get());
+        } else {
+            return ResponseEntity.status(404).body("Student not found with rollNo: " + rollNo);
+        }
     }
 
 }
