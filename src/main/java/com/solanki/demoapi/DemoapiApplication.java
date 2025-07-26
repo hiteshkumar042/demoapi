05a6fa7fc97a93c2a2960b2a3fb25013f59f57cf
hiteshkumar042 <hksolanki78@gmail.com>
2025-07-26 17:50:00 +0530

    env file issue

--- src/main/java/com/solanki/demoapi/DemoapiApplication.java
+++ src/main/java/com/solanki/demoapi/DemoapiApplication.java
@@ -8,10 +8,6 @@ import io.github.cdimascio.dotenv.Dotenv;
 public class DemoapiApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
-		System.setProperty("DATABASE_URL", dotenv.get("DATABASE_URL"));
-		System.setProperty("DATABASE_USER", dotenv.get("DATABASE_USER"));
-		System.setProperty("DATABASE_PASSWORD", dotenv.get("DATABASE_PASSWORD"));
 		SpringApplication.run(DemoapiApplication.class, args);
 	}
 
